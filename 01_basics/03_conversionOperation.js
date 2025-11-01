57e5b91650f0fe247d41b0d41adbe2aa9a9e8415
Satyam Mishra <71537294+ssatyam05@users.noreply.github.com>
2025-11-01 17:34:23 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+// "33" => 33
+// "331bc" => NaN
+// true => 1; false=0;
+
+
+let isLoggedin = 1;
+let booleanIsLoggedIn = Boolean(isLoggedin);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false;
+// "" => false;
+// "hitesh" => true;
+
+let someNumber = 33;
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
